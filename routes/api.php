1ea4c0edbd82d9a00c6bee9d4ddc473949ed7fbd
kadirtikil <kadirtikil@gmail.com>
2024-06-17 23:32:39 +0200

    just some notes. will handle auth next.

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,18 @@ Route::get('/user', function (Request $request) {
 //////////////////////////////////////////////////////////////////////////////
 // NOTE: all these routes have to pass through a middleware to be checked. otherwise it would cause trouble, making 
 // it possible to manipulate the database without authentication by just forging the urls. this is  not bonita!
-// The authentication itself might be by my implementation, or i might just use sanctum or something. dont know yet.
+// The authentication itself might be by my implementation, or i might just use sanctum or facade or something. dont know yet.
+
+// Ok the Auhtentication is a little more complex than originally expected. Not because of the implementation but because of 
+// data safety in germany. i dont want to catch a law suit becuase of this stuff so im just providing a couple log in accounts 
+// to anyone who wants to check out my projects. that way i dont have to deal with this stuff.
+
+// So i will setup like 5 default users with the email provided on the website such that they can be used. or i might just make it 
+// such that a complaint on the website cant be edited and i therefore dont have ti safe the email persistently. 
+
+// then the deletion would take place with a password type thingy such that it can be used to delete a post. 
+// the password is then provided to the user and associated with the post in the db.... hmmmmmmm have to sleep about that stuff.
+
 
 // test
 Route::get('test', [PostController::class, 'test']);
